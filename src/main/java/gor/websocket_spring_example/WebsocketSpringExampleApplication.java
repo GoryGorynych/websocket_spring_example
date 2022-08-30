166f7375a393e734d0535d0947d674218ab1cb8d
Vladimit <gory.gorynych@gmail.com>
2022-08-30 10:39:40 +0300

    first commit

---
+++ src/main/java/gor/websocket_spring_example/WebsocketSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package gor.websocket_spring_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketSpringExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gor/websocket_spring_example/WebsocketSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package gor.websocket_spring_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSpringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
