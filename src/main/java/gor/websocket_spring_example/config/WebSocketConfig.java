759cec245391c4638e27b0d4e92d04dbb9966874
Vladimir Gorenkov <gorenkov@lt-m.ru>
2022-08-30 19:16:34 +0300

    commit

--- src/main/java/gor/websocket_spring_example/config/WebSocketConfig.java
+++ src/main/java/gor/websocket_spring_example/config/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package gor.websocket_spring_example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,7 +26,18 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+        System.out.println("registerStompEndpoints test");
+        registry.addEndpoint("/chat").setHandshakeHandler(new DefaultHandshakeHandler() {
+
+            //Get sessionId from request and set it in Map attributes
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+                                           Map attributes) throws Exception {
+                if (request instanceof ServletServerHttpRequest) {
+                    ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+                    HttpSession session = servletRequest.getServletRequest().getSession();
+                    attributes.put("sessionId", session.getId());
+                }
+                return true;
+            }}).withSockJS();
     }
 }
--- src/main/java/gor/websocket_spring_example/contoller/ChatController.java
+++ src/main/java/gor/websocket_spring_example/contoller/ChatController.java
@@ -1,19 +1,35 @@
 package gor.websocket_spring_example.contoller;
 
+import com.google.gson.Gson;
 import gor.websocket_spring_example.model.Message;
 import gor.websocket_spring_example.model.OutputMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Controller
 public class ChatController {
+
+    private Gson gson = new Gson();
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     public OutputMessage send(Message message) throws Exception {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
-        return new OutputMessage(message.getFrom(), message.getText(), time);
+        return new OutputMessage(message.getFrom().toUpperCase(), "pong", time);
     }
+
+//    @MessageMapping("/chat")
+//    @SendToUser("/topic/messages")
+//    public String send(
+//        @Payload String message,
+//        Principal principal) throws Exception {
+//            return gson
+//                    .fromJson(message, Map.class)
+//                    .get("name").toString();
+//    }
+
 }
